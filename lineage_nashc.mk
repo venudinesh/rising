efee18d6c3028b7154edfa0d4220b0422e23e32d
venudinesh <venudinesh21@gmail.com>
2023-06-12 23:13:55 +0530

    RISE

--- lineage_nashc.mk
+++ lineage_nashc.mk
@@ -15,6 +15,23 @@ $(call inherit-product, device/realme/nashc/device.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Inherit some RisingOSS stuff.
+RISING_CHIPSET := "Mediatek Helio G95"
+RISING_MAINTAINER := Eren_Yeager
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+RISING_PACKAGE_TYPE := "CORE GAPPS"
+TARGET_ENABLE_BLUR := true
+TARGET_HAS_UDFPS := true
+SUSHI_BOOTANIMATION := 1080
+WITH_GMS := true
+TARGET_CORE_GMS := true
+TARGET_USE_GOOGLE_TELEPHONY := fals
+TARGET_CORE_GMS_EXTRAS := true
+TARGET_USE_PIXEL_FINGERPRINT := true
+TARGET_EXCLUDES_AUDIOFX := false
+TARGET_BUILD_APERTURE_CAMERA := true
+
 PRODUCT_NAME := lineage_nashc
 PRODUCT_DEVICE := nashc
 PRODUCT_MANUFACTURER := Realme
@@ -29,18 +46,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="RMX3085-user 12 SP1A.210812.016 R.GDPR.c3619e_1 release-keys"
 
 BUILD_FINGERPRINT := realme/RMX3085/RMX3085L1:12/SP1A.210812.016/R.GDPR.c3619e_1:user/release-keys
-
-# MISC stuff
-TARGET_BOOT_ANIMATION_RES := 1080
-ALLOW_MISSING_DEPENDENCIES=true
-TARGET_USE_GOOGLE_TELEPHONY := false
-TARGET_ENABLE_BLUR := true
-TARGET_INCLUDE_MATLOG := false
-TARGET_USE_PIXEL_LAUNCHER := false
-TARGET_FACE_UNLOCK_SUPPORTED := true
-
-#FOD Animations
-TARGET_HAS_UDFPS := true
-
-#Maintainer
-ALPHA_MAINTAINER := Eren_Yeager
